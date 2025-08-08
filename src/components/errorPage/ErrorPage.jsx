f0a74806b4ad87c9e79fc43c2eab64c79161f3cf
Nahid-07 <nahidasif1998@gmail.com>
2025-08-08 17:57:53 +0600

    error page modified

--- src/components/errorPage/ErrorPage.jsx
+++ src/components/errorPage/ErrorPage.jsx
@@ -1,7 +1,10 @@
+import { Link } from "react-router";
+
 export const ErrorPage = () => {
   return (
     <div className="flex items-center justify-center h-screen w-full">
       PAGE NOT FOUND
+       <Link to={'/'}>go home</Link>
     </div>
   );
 };
