5d9b82dd16318781b4f76549fd806d0c357a7e57
Nahid-07 <nahidasif1998@gmail.com>
2025-08-11 16:49:10 +0600

    sort functionality added

--- src/components/listed-books/Listed_books.jsx
+++ src/components/listed-books/Listed_books.jsx
@@ -9,7 +9,7 @@ import { WishListedBooks } from "./WishListedBooks";
 
 export const Listed_books = () => {
   const [readBooks, setReadBooks] = useState([]);
-  const [wishlists, setWishList] = useState([])
+  const [wishlists, setWishList] = useState([]);
   const allBooks = useLoaderData();
   useEffect(() => {
     const readBooksList = getStoredReadList();
@@ -20,17 +20,40 @@ export const Listed_books = () => {
     setReadBooks(booksWhichRead);
   }, [allBooks]);
 
-  useEffect(()=>{
+  useEffect(() => {
     const wishListBook = getWishListFrmLS();
-    const wishlist = wishListBook.map(id => parseInt(id));
-    const bookWhichWishlisted = allBooks.filter(book => wishlist.includes(book.bookId))
-    setWishList(bookWhichWishlisted)
-  },[allBooks])
-  
+    const wishlist = wishListBook.map((id) => parseInt(id));
+    const bookWhichWishlisted = allBooks.filter((book) =>
+      wishlist.includes(book.bookId)
+    );
+    setWishList(bookWhichWishlisted);
+  }, [allBooks]);
+
+  const handleSort = (e) => {
+  const value = e.target.value;
+
+  if (value === "rating") {
+    const sorted = [...readBooks].sort((a, b) => b.rating - a.rating);
+    setReadBooks(sorted);
+  } 
+  else if (value === "pages") {
+    const sorted = [...readBooks].sort((a, b) => b.totalPages - a.totalPages);
+    setReadBooks(sorted);
+  }
+};
+
+
   return (
     <div>
       <div className="bg-gray-300 text-center font-bold text-xl p-4 rounded-md my-5">
         Books
+      </div>
+      <div className="flex justify-center my-5">
+        <select onChange={handleSort} className="text-center appearance-none bg-white border border-gray-300 rounded-lg px-4 py-2 pr-8 shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 text-gray-700 cursor-pointer">
+          <option value="">Sort By</option>
+          <option value="rating">Rating</option>
+          <option value="pages">Number of Pages</option>
+        </select>
       </div>
       <Tabs>
         <TabList>
